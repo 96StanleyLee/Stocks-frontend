1929920b7b9409c6a615d8edbce2543aa1eec7e2
96StanleyLee <96stanleylee@gmail.com>
2020-06-22 12:48:29 -0400

    added loader

--- src/container/Portfolio.js
+++ src/container/Portfolio.js
@@ -18,25 +18,14 @@ const useStyles = makeStyles({
     return { ticker, current, previous, change};
   }
   
-  // const rows = [
-  // createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  //   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  //   createData('Eclair', 262, 16.0, 24, 6.0),
-  //   createData('Cupcake', 305, 3.7, 67, 4.3),
-  //   createData('Gingerbread', 356, 16.0, 49, 3.9),
-  // ];
-
 
 const Portfolio = (props) =>{
     const classes = useStyles();
 
-    // const rows = [
-    //   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    // ]
 
     let [portfolio, setPortfolio] = useState([])
     let [rows, setRows] = useState([])
-    let [loading, setLoading] = useState(false)
+    let [loading, setLoading] = useState(true)
 
 
     const createRows = () =>{
@@ -45,7 +34,6 @@ const Portfolio = (props) =>{
         console.log(stock)
         array.push(createData(stock[0],stock[1], stock[2]))
       })
-
       setRows(array)
     }
 
@@ -62,15 +50,17 @@ const Portfolio = (props) =>{
             array.push([data.meta.symbol,data.values[0].close,secondData.values[0].open])
           }    
           setPortfolio(array)
+          setLoading(false)
         }
+        createRows()
       }
 
       fetchStock()
     },[props.portfolio])
 
-    useEffect(()=>{
-      createRows()
-    },[portfolio])
+    // useEffect(()=>{
+    //   createRows()
+    // },[portfolio])
 
     
 
@@ -80,17 +70,15 @@ const Portfolio = (props) =>{
 
 
     return(
-        <TableContainer style={{margin:'auto',width: '80%'} }component={Paper}>
+      <>{loading? <div class="lds-ring"><div></div><div></div><div></div><div></div></div>:
+        <TableContainer style={{margin: 'auto',width: '80%'} }component={Paper}>
       <Table  className={classes.table} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>Ticker</TableCell>
-            {/* <TableCell align="right">Ticker</TableCell> */}
             <TableCell align="right">Current</TableCell>
             <TableCell align="right">Previous</TableCell>
             <TableCell align="right">Change</TableCell>
-
-            {/* <TableCell align="right">Change&nbsp;(g)</TableCell> */}
           </TableRow>
         </TableHead>
         <TableBody>
@@ -99,12 +87,11 @@ const Portfolio = (props) =>{
               <TableCell component="th" scope="row">
                 {row.ticker}
               </TableCell>
-              {/* <TableCell align="right">{row.ticker}</TableCell> */}
               <TableCell align="right">{row.current}</TableCell>
               <TableCell align="right">{row.previous}</TableCell>
               {row.current - row.previous > 1?
-                            <TableCell align="right" style={{color: 'green'}}>{(row.current - row.previous)} green </TableCell>
-                            :<TableCell align="right" style={{color: 'red'}}>{(row.current - row.previous)} red </TableCell>
+                            <TableCell align="right" style={{color: 'green'}}>{(row.current - row.previous).toFixed(4)}%</TableCell>
+                            :<TableCell align="right" style={{color: 'red'}}>{(row.current - row.previous).toFixed(4)}%</TableCell>
 
                           }
             </TableRow>
@@ -112,6 +99,8 @@ const Portfolio = (props) =>{
         </TableBody>
       </Table>
     </TableContainer>
+}
+    </>
     )
 }
 
